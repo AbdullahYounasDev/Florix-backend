85383f5c12f5e3add7eff04f6f199ff7eaeb7157
AbdullahYounasDev <ab.khokhar2005@gmail.com>
2026-01-28 21:18:42 +0500

    Extend limit of backend space

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(cors({
 }));
 
 // Parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
 
 // API routes
 app.use('/api/v1', routes);

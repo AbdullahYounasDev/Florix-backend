810005002289ba9d5a066e854d40709b3a1487ce
AbdullahYounasDev <ab.khokhar2005@gmail.com>
2026-01-28 20:50:41 +0500

    Update imageAnalysis fucntion

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -31,52 +31,20 @@ export const getAiResponse = async (req, res) => {
 };
 
 export const getImageAnalysis = async (req, res) => {
-  const file = req.file;
-  let { country } = req.body;
+  const {imageData, prompt} = req.body;
 
-  if (!file) {
-    return error(res, ErrorCodes.IMAGE_REQUIRED, 400);
+  if(!imageData){
+    return error (res, ErrorCodes.IMAGE_DATA_REQUIRED, 400);
+  }
+  if(!prompt){
+    return error (res, ErrorCodes.PROMPT_REQUIRED, 400);
   }
 
-  if (!country || country.trim() === "") {
-    country = "International";
-  }
+  const data = await imageAnalysisService(imageData, prompt)
 
-  let publicId;
-
-  try {
-    const uploaded = await uploadImage(file.buffer);
-    if (!uploaded.success) {
-      return error(res, ErrorCodes.IMAGE_UPLOAD_FAILED, 500);
-    }
-    
-    const imageUrl = uploaded.url;
-    publicId = uploaded.publicId;
-
-    const base64Image = await imageUrlToBase64(imageUrl);
-
-    const imageData = {
-      inlineData: {
-        data: base64Image,
-        mimeType: file.mimetype,
-      },
-    };
-
-    const prompt = ImageAnalysisPrompt(country);
-    const data = await imageAnalysisService(imageData, prompt);
-
-    return res.status(200).json({
-      success: true,
-      data,
-    });
-
-  } finally {
-    if (publicId) {
-      const deleted = await deleteImage(publicId);
-      if (!deleted?.success) {
-        console.error("Cloudinary cleanup failed:", deleted?.error);
-      }
-    }
-  }
+  return res.status(200).json({
+    success: true,
+    data
+  })
 };
 
--- routes/ai.route.js
+++ routes/ai.route.js
@@ -2,23 +2,12 @@ import express from "express";
 const router = express.Router();
 import {getAiResponse, getImageAnalysis} from '../controllers/ai.controller.js';
 import { asyncHandler } from "../utils/asyncHandler.js"
-import multer from "multer";
 import { ErrorCodes } from "../utils/constants.js";
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error(ErrorCodes.INVALID_FILE_TYPE), false);
-    }
-  }
-});
+
 
 
 router.post('/getAiResponse', asyncHandler(getAiResponse));
-router.post('/getImageAnalysis', upload.single('imagePath'), asyncHandler(getImageAnalysis));
+router.post('/getImageAnalysis', asyncHandler(getImageAnalysis));
 
 export default router;
